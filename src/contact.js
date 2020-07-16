6e8b405f38bb39c13c9be49f8a11ac8541d9dc7f
Kathlia Carlos <lia.carlos23@gmail.com>
2020-07-16 20:27:24 +0800

    add main elements for contacts

--- src/contact.js
+++ src/contact.js
@@ -1,1 +1,39 @@
-//import "./contactStyle.css"
+import "./contactStyle.css"
+import {createTags} from "./homepage"
+
+
+function initialPageLoad() {
+
+    //elements 
+    let container = createTags("div", "container")
+
+    let header = createTags("div", "header")
+    let home = createTags("div")
+    let menu = createTags("div")
+    let contact = createTags("div")
+
+    let main = createTags("div", "main")
+    let info = createTags("div", "info")
+    let title = createTags("h1")
+
+
+
+    //text
+    home.innerHTML = "Home";
+    menu.innerHTML = "Menu"
+    contact.innerHTML = "Contact"
+    title.innerHTML = "Contact Me"
+
+
+    //append
+    info.append(title)
+    main.append(info)
+    header.append(home, menu, contact)
+    container.append(header, main)
+
+
+    return container
+}
+
+
+export {initialPageLoad as initialPage}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import * as homepage from "./homepage"
 import * as menu from "./menu"
+import * as contact from "./contact"
 
 
 const content = document.querySelector("#content")
@@ -8,3 +9,5 @@ const content = document.querySelector("#content")
 //content.append(homepage.initialpage())
 
 //content.append(menu.initialPage())
+
+content.append(contact.initialPage())
